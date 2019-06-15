5aaae786ea17e9675603e05efb087cb46ccc04aa
pluraliseseverythings <domenico.corapi@babylonehalth.com>
2019-06-15 14:21:26 +0100

    Attempting flask alexa

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+from flask_ask import Ask, statement, question, session
 
 
 @app.route("/")
@@ -13,7 +14,13 @@ def hello():
 def alexa():
     return "Hello from Alexa!"
 
+@ask.intent("PrescriptionCost")
+def prescription_cost():
+    return statement("response to prescription cost")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+
